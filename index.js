1ac30ae686e5b6ba02796d1a4a8176edffcf8ad9
Alix Axel <alix.axel@gmail.com>
2015-08-06 06:06:08 +0200

    fixed indentation and added forOwn() helper

--- index.js
+++ index.js
@@ -43,70 +43,52 @@ module.exports = (function () {
 		var delta = 1,
 			inverse = 1 / self.count;
 
-		for (var source in self.edges) {
-			if (self.edges.hasOwnProperty(source) === true) {
-				if (self.nodes[source].outbound > 0) {
-					for (var target in self.edges[source]) {
-						if (self.edges[source].hasOwnProperty(target) === true) {
-							self.edges[source][target] /= self.nodes[source].outbound;
-						}
-					}
-				}
+		forOwn(self.edges, function (source) {
+			if (self.nodes[source].outbound > 0) {
+				forOwn(self.edges[source], function (target) {
+					self.edges[source][target] /= self.nodes[source].outbound;
+				});
 			}
-		}
+		});
 
-		for (var key in self.nodes) {
-			if (self.nodes.hasOwnProperty(key) === true) {
-				self.nodes[key].weight = inverse;
-			}
-		}
+		forOwn(self.nodes, function (key) {
+			self.nodes[key].weight = inverse;
+		});
 
 		while (delta > epsilon) {
 			var leak = 0,
 				nodes = {};
 
-			for (var key in self.nodes) {
-				if (self.nodes.hasOwnProperty(key) === true) {
-					nodes[key] = self.nodes[key].weight;
+			forOwn(self.nodes, function (key, value) {
+				nodes[key] = value.weight;
 
-					if (self.nodes[key].outbound === 0) {
-						leak += self.nodes[key].weight;
-					}
-
-					self.nodes[key].weight = 0;
+				if (value.outbound === 0) {
+					leak += value.weight;
 				}
-			}
+
+				self.nodes[key].weight = 0;
+			});
 
 			leak *= alpha;
 
-			for (var source in self.nodes) {
-				if (self.nodes.hasOwnProperty(source) === true) {
-					for (var target in self.edges[source]) {
-						if (self.edges[source].hasOwnProperty(target) === true) {
-							self.nodes[target].weight += alpha * nodes[source] * self.edges[source][target];
-						}
-					}
+			forOwn(self.nodes, function (source) {
+				forOwn(self.edges[source], function (target, weight) {
+					self.nodes[target].weight += alpha * nodes[source] * weight;
+				});
 
-					self.nodes[source].weight += (1 - alpha) * inverse + leak * inverse;
-				}
-			}
+				self.nodes[source].weight += (1 - alpha) * inverse + leak * inverse;
+			});
 
 			delta = 0;
 
-			for (var key in self.nodes) {
-				if (self.nodes.hasOwnProperty(key) === true) {
-					delta += Math.abs(self.nodes[key].weight - nodes[key]);
-				}
-			}
+			forOwn(self.nodes, function (key, value) {
+				delta += Math.abs(value.weight - nodes[key]);
+			});
 		}
 
-		for (var key in self.nodes) {
-			if (self.nodes.hasOwnProperty(key) === true) {
-				if (callback(key, self.nodes[key].weight) === false) {
-					break;
-				}
-			}
-		}
+		forOwn(self.nodes, function (key) {
+			return callback(key, self.nodes[key].weight);
+		});
 	};
 
 	self.reset = function () {
@@ -116,3 +98,15 @@ module.exports = (function () {
 
 	return self;
 })();
+
+function forOwn(object, callback) {
+	if ((typeof object === 'object') && (typeof callback === 'function')) {
+		for (var key in object) {
+			if (object.hasOwnProperty(key) === true) {
+				if (callback(key, object[key]) === false) {
+					break;
+				}
+			}
+		}
+	}
+}
